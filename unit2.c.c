9c0b024f6d4265b1681478481c4d73a574c51bf9
paul26214 <kimani.26214@students.kyu.ac.ke>
2025-09-19 12:47:45 +0300

    Add files via upload

---
+++ unit2.c.c
@@ -0,0 +1,19 @@
+/*
+name:paul mutua
+reg:CT100/G/26214/25
+Description:Display of all units in IT
+*/
+
+#include<stdio.h>//pre-processor directive printf()
+
+int main(){
+	printf("unit\t Description \t\n");
+	printf("SPH2107\t Analogue Electronics \t\n");
+	printf("SPI2102\t Computer system and orgarnisation \t\n");
+	printf("SPI2101\t Introduction to programming and Algorithms \t\n");
+	printf("SPI2103\t Introduction tocomputer application \t\n");
+	printf("SPI2120\t Essentials of accounting \t\n");
+	printf("SPM2104\t Mathematics for science \t\n");
+	printf("UCU2105\t Fundermentals of ICT \t\n");
+	printf("UCU2106\t communication and information literacy \t\n");
+}
